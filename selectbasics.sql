b2bc45add0c1ca15586be1de91444fecccea0fb9
Hudson Bezerra <hudsonbezerra77@gmail.com>
2025-06-02 16:28:35 -0300

    Create selectbasics.sql

---
+++ selectbasics.sql
@@ -0,0 +1,22 @@
+1-
+SELECT population FROM world
+  WHERE name = 'Germany'
+2-
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Norway','Denmark');
+3-
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000  AND 250000
+4-
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+5-
+SELECT name FROM world
+  WHERE name LIKE 'y%'
+6-
+SELECT name FROM world
+  WHERE name LIKE 'x%'
+7-
+  SELECT name FROM world
+  WHERE name LIKE 'land%'
+
